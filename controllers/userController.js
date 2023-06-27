676eaf929bfaf3a19cb0bdc81c8f2464b87898f8
zeuz <zeuz@zeuzs-iMac.local>
2023-06-27 15:18:44 -0400

    userController setup

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const { User } = require("../models")
+
+const userController = {
+    getAllUsers(req, res) {
+
+    },
+    getUserById(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriends(req, res) {
+
+    },
+    deleteFriends(req, res) {
+
+    }
+}
+
+module.exports = userController;
