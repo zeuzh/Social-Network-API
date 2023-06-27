fdea1977e0d36c3d987083e8d520d001b4fe701c
zeuz <zeuz@zeuzs-iMac.local>
2023-06-27 15:16:48 -0400

    thoughtController set up

---
+++ controllers/thoughtController.js
@@ -0,0 +1,27 @@
+const {User, Thought} = require("../models")
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+
+    },
+    getThoughtById(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    addReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    }
+}
+
+module.exports = thoughtController;
